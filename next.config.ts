cfdef2a51e0a51e56bcb150f68df13283fb983bf
DawsonCoats <decoats18@gmail.com>
2026-03-17 14:33:47 -0400

    Fix asset prefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/haven',
-  assetPrefix: '/haven',
+  assetPrefix: 'https://dawsoncoats.github.io/haven',
   images: {
     unoptimized: true,
   },
